fad6c0756c6fdfe814e433d9de71977fd6b785f3
1402Diana <diana.shagiakhmetova@gmail.com>
2024-05-25 14:59:02 +0200

    Lab 2 SQL

---
+++ Lab 2.sql
@@ -0,0 +1,9 @@
+-- use sakila
+select * from actor;
+select * from film;
+select * from customer;
+select title from film;
+select distinct language_id as language from film;
+select count(*) as store_count from store;
+select count(*) as employee_count from staff;
+select first_name from st aff
